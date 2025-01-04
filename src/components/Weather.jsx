44a7d94e2f554d6d6d7d1d59949d3fb7b4ce61d7
pratimaasapkota <pratimasapkota509@gmail.com>
2025-01-03 23:44:42 -1100

    adding api

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,15 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './Weather.css'
 import search_icon from '../assets/search.png'
 import clear_icon from '../assets/clear.png'
-// import cloud_icon from '../assets/cloud.png'
-// import drizzle_icon from '../assets/drizzle.png'
-// import rain_icon from '../assets/rain.png'
-// import snow_icon from '../assets/snow.png'
+import cloud_icon from '../assets/cloud.png'
+import drizzle_icon from '../assets/drizzle.png'
+import rain_icon from '../assets/rain.png'
+import snow_icon from '../assets/snow.png'
 import wind_icon from '../assets/wind.png'
 import humidity_icon from '../assets/humidity.png'
 
 const Weather = () => {
+  const search = async (city)=>{
+    try {
+      const url =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${import.meta.env.VITE_APP-ID}`;
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      
+    }
+  }
+  useEffect(()=>{
+    search("London");
+  },[])
+
   return (
     <div className='weather'>
       <div className="search-bar">
