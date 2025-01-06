425cb1b5e9e4579e396f8e302d5584ea1c4c9f41
pratimaasapkota <pratimasapkota509@gmail.com>
2025-01-05 14:32:52 -1100

    humidity

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -6,6 +6,7 @@ import clear_icon from '../assets/clear.png'
 // import drizzle_icon from '../assets/drizzle.png'
 // import rain_icon from '../assets/rain.png'
 // import snow_icon from '../assets/snow.png'
+
 import wind_icon from '../assets/wind.png'
 import humidity_icon from '../assets/humidity.png'
 
