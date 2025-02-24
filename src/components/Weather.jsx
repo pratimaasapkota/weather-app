7a49fb4d4944116856b6befe4b2ee00e17415ee5
pratimaasapkota <pratimasapkota509@gmail.com>
2025-02-24 11:04:01 -1100

    code modify

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -35,12 +35,7 @@ const Weather = () => {
       const temperature = Math.floor(data.current.temp_c);
 
       
-      setWeatherData({
-        temperature: temperature,
-        location: data.location.name,
-        humidity: data.current.humidity,
-        windSpeed: data.current.wind_kph,
-        icon: getWeatherIcon(condition, temperature),
+      
       });
 
       setError(null); // Clear any previous error
