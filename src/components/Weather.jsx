e6cceee13d86634efb6849d14ef4608185b24d8b
pratimaasapkota <pratimasapkota509@gmail.com>
2025-02-25 11:37:39 -1100

    code modify

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -30,7 +30,19 @@ const Weather = () => {
       if (!response.ok) {
         throw new Error("Location not found");
       }
+      const data = await response.json();
+      const condition = data.current.condition.text.toLowerCase();
+      const temperature = Math.floor(data.current.temp_c);
+
       
+      setWeatherData({
+        temperature: temperature,
+        location: data.location.name,
+        humidity: data.current.humidity,
+        windSpeed: data.current.wind_kph,
+        icon: getWeatherIcon(condition, temperature),  
+      });
+
       setError(null); // Clear any previous error
     } catch (error) {
       console.error('Error fetching weather data:', error);
